78f9f78f87ddecba9057b2ff6a645c96fdf48cdd
Muhammed Yasin Bölük <myasinboluk@gmail.com>
2022-11-17 14:24:32 +0300

    Added inline function & call back exercise

--- Exercise_3/Exercise_3.cpp
+++ Exercise_3/Exercise_3.cpp
@@ -1,6 +1,6 @@
 ﻿//Functions and factorial
 #include <iostream>
-
+#include <string>
 
 void mesaj(char msg,char msg2) {
 	std::cout << msg<<msg2<<std::endl;
@@ -12,10 +12,14 @@ int topla(int sayi1, int sayi2) {
 	return sayi1 + sayi2;
 
 }
-
 //////////////////
+void print(std::string text="\n",int=0,bool as=false) {
+	std::cout << text;
+}
 
 
+
+///////////////////////////
 int fakt(int sayi) {
 	int i = 1;
 	int toplam=1;
@@ -27,6 +31,23 @@ int fakt(int sayi) {
 	}
 	return toplam;
 }
+/////////////////
+int carp(int p, int l) {
+	return p * l;
+}
+
+int getir(int(*fint)(int, int)) {
+	int p = fint(10, 10);
+	return p;
+
+}
+//////////////////
+inline int bol(int e, int r) {
+	return e / r;
+}
+
+
+//////////////////////////
 
 int main()
 {
@@ -36,6 +57,12 @@ int main()
 	std::cout << "Bir sayi giriniz";
 	std::cin >> sayi;
 	
-	std::cout << sayi, std::cout << "!=" , std::cout << fakt(sayi);
-
+	std::cout << sayi, std::cout << "!=" , std::cout << fakt(sayi)<<std::endl;
+	
+	print();
+	int l = getir(carp);
+	std::cout << l<<std::endl;
+	std::cout << bol(10, 2);
 }
+//////////////////////////////////////////////// 
+
