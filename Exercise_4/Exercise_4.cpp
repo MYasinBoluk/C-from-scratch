f0813304cd1d15678af08b7501b58cd982edeabd
Muhammed Yasin Bölük <myasinboluk@gmail.com>
2022-11-17 18:17:35 +0300

    Exercise about For,if,while and switch

---
+++ Exercise_4/Exercise_4.cpp
@@ -0,0 +1,73 @@
+﻿// Exercise_4.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    char islem = '+';
+    while (true) {
+        std::cout << "Birinci sayiyi giriniz:" << std::endl;
+
+        std::cin >> a;
+        std::cout << "ikinci sayiyi giriniz:" << std::endl;
+
+        std::cin >> b;
+        std::cout << "Yapilicak islemi giriniz" << std::endl;
+        std::cin >> islem;
+
+       /* if (islem == 'e') {
+            continue;
+        }
+        if (islem == 'e') {
+            break;
+        }*/
+
+        if (islem == 'e') {
+            goto stop;
+        }
+
+
+
+        switch (islem) {
+        case '+':
+            std::cout << "Sonuc:" << a + b;
+            break;
+        case '-':
+            std::cout << "Sonuc:" << a - b;
+            break;
+        case '*':
+            std::cout << "Sonuc:" << a * b;
+            break;
+        case '/':
+            std::cout << "Sonuc:" << a / b;
+            break;
+        default:
+            std::cout << "Hatali islem secimi";
+            break;
+
+        }
+
+
+    } 
+    stop:
+    std::cout<<"Durdu";
+
+    /*for (int i = 1; i <= 10; i++)
+    {
+        std::cout << "Dongu" << i<<std::endl;
+    }*/
+
+   
+
+    /*if (islem == '+') {
+        std::cout << "Sonuc:" << a + b;
+
+    }
+    else {
+        std::cout << "islem yok";
+    }*/
+
+}
