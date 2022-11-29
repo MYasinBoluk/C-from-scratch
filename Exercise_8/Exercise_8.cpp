6279a29f866c2f7b41ae4c33c0b6ecff216af04d
Muhammed Yasin Bölük <myasinboluk@gmail.com>
2022-11-29 19:08:14 +0300

    Example for static

---
+++ Exercise_8/Exercise_8.cpp
@@ -0,0 +1,31 @@
+﻿// Exercise_8.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+class Kitap {
+public:
+	static int adet;
+	Kitap() {
+		adet++;
+
+	}
+	static int get() {
+		return adet;
+
+	}
+};
+int Kitap::adet = 0;
+
+
+int main()
+{
+	Kitap kitap;
+	Kitap kitap2;
+	Kitap kitap3;
+	Kitap kitap4;
+	Kitap kitap5;
+	kitap5.get();
+	std::cout << Kitap::get();
+
+}
