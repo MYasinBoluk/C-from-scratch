7ae9a2beb603f75a4e0d95e8f1f498694264b7f9
Muhammed Yasin Bölük <myasinboluk@gmail.com>
2022-11-29 18:32:44 +0300

    Example for Abstraction

---
+++ Exercise_7/Exercise_7.cpp
@@ -0,0 +1,53 @@
+﻿// Exercise_7.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+class Hayvan {
+public:
+	virtual void sescal() = 0;
+	
+};
+
+class Kopek:public Hayvan {
+public:
+	void sescal() {
+		std::cout << "Hav";
+	}
+};
+
+class Kedi :public Hayvan {
+public:
+	void sescal() {
+		std::cout << "Miyav";
+	}
+};
+
+class Ordek :public Hayvan {
+public:
+	void sescal() {
+		std::cout << "Hav";
+	}
+};
+
+class Fare :public Hayvan {
+public:
+	void sescal() {
+		std::cout << "vik";
+	}
+};
+
+void tumsescal(Hayvan* hptr) {
+	hptr->sescal();
+
+}
+
+int main()
+{
+	Kopek kopek;
+	Kedi kedi;
+	Ordek ordek;
+	Fare fare;
+	tumsescal(&fare);
+}
+
