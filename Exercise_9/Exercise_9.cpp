ff7d8b4173ce4c47aea3e3310b5bc16faa9d8ee6
Muhammed Yasin Bölük <myasinboluk@gmail.com>
2022-11-29 19:25:43 +0300

    Using friend class and function

---
+++ Exercise_9/Exercise_9.cpp
@@ -0,0 +1,46 @@
+﻿// Exercise_9.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+class Payment {
+	private:
+		float tutar=0;
+	public:
+		std::string paraBirimi;
+
+		void setTutar(float t) {
+			if (t > 0) {
+				tutar = t;
+
+			}
+			else {
+				tutar = 0.1;
+			}
+		}
+		void pay() {
+			std::cout << tutar << ":Odeme alindi";
+		}
+		//friend void degistir(Payment* paymentPtr);
+		friend class Test;
+};
+//void degistir(Payment* paymentPtr) {
+//	paymentPtr->tutar = -10;
+//}
+///////////////////////////////////////////////
+class Test {
+public:
+	void degistir(Payment* payment) {
+		payment->tutar = -10;
+	}
+};
+
+int main()
+{
+	Payment payment;
+	/*degistir(&payment);*/
+	Test test;
+	test.degistir(&payment);
+	payment.paraBirimi = "tl";
+	payment.pay();
+}
